c958084da067dd80a91258f7c4c2486b8d338f38
Felipe-Shalon <felipeschunkoficial@hotmail.com>
2025-09-01 21:52:19 -0300

    Add files via upload

---
+++ bolo.js
@@ -0,0 +1,20 @@
+// Fazendo bolo de três camadas de recheio
+primeiroPasso = "1. Pegue os igredientes e os itens de produção de acordo com a descrição do livro \n"
+segundoPasso = "2. Pegue os itens de produção e comece preparando os três recheios \n"
+terceiroPasso = "3. Coloque os recheios na geladeira conforme for ficando pronto \n"
+quartoPasso= "4. Inicie a produção da massa do bolo \n"
+quintoPasso = "5. Após a massa ficar pronta, pegue quatro formas e coloque papel manteiga e unte as laterais com manteiga ou óleo \n"
+sextoPasso = "6. Pegue o peso total da massa e divide em quatro partes e coloque uma em cada forma \n"
+sétimoPasso = "7. Pré-aqueça o forno a 180°C entre 10 a 15 minutos e deixa a massa parada crescendo \n"
+oitavoPasso = "8. Após o forno aquecer, coloque as formas no forno com a mesma temperatura por 30 a 40 minutos \n"
+nonoPasso = "9. Após a massa ficar pronta, retire ela do forno \n"
+decimoPasso = "10. Retire os recheios da geladeira e as massas das formas \n"
+decimoPrimeiroPaaso = "11. Pegue três massas e coloque um recheio em cada \n"
+decimoSegundoPasso = "12. Junte as massas uma sobre a outra \n"
+decimoTerceiroPasso = "13. Coloque duas caixas de chantilly no liquidifiador e bata até ficar homogênio \n"
+decimoQuartoPasso = "14. Após o chantilly ficar homegênio, pega uma espátula e passe ao redor do bolo \n"
+decimoQuintoPasso = "15. Coloque o bolo na geladeira até a estrutura ficar firme \n"
+decimoSextoPasso = "16. Após a estrutura ficar firme, o bolo está pronto para ser servido \n"
+
+ // utilizando o console.log para exibir as informações
+console.log(primeiroPasso,segundoPasso,terceiroPasso,quartoPasso,quintoPasso,sextoPasso,sétimoPasso,oitavoPasso,nonoPasso,decimoPasso,decimoPrimeiroPaaso,decimoSegundoPasso,decimoTerceiroPasso,decimoQuartoPasso,decimoQuintoPasso,decimoSextoPasso)
