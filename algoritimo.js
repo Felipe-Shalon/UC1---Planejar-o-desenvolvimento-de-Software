4fd98bdb1e459f12671154d5df461827f31a4195
Felipe-Shalon <felipeschunkoficial@hotmail.com>
2025-09-01 21:54:46 -0300

    Add files via upload

---
+++ algoritimo.js
@@ -0,0 +1,12 @@
+// Fazendo café
+primeiroPasso = "1. Coloque água na chaleira \n"
+segundoPasso = "2. Aqueça a água no fogão até ferver \n"
+terceiroPasso = "3. Prepare o coador com o filtro de café \n"
+quartoPasso= "4. Coloque o café dentro do filtro \n"
+quintoPasso = "5. Coloque a água já fervida no coador \n"
+sextoPasso = "6. Espere coar o café \n"
+sétimoPasso = "7. Pegue uma xícara \n"
+oitavoPasso = "8. Sirva-se \n"
+ 
+// utilizando o console.log para exibir as informações
+console.log(primeiroPasso,segundoPasso,terceiroPasso,quartoPasso,quintoPasso,sextoPasso,sétimoPasso,oitavoPasso)
